20b966baeb7df5abb176cde52ffd3d8b34f20305
hayat4144 <ihayat855@gmail.com>
2022-11-07 21:33:38 +0530

    footer route is removed.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import './index.css'
 import Signup from "./Auth/Signup";
 import Signin from "./Auth/Signin";
-import Navbar from "./MainComponent/Navbar";
+import Navbar from "./Component/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Password_change from './settings/Password_change';
+
 function App() {
   return (
     <div className='app'>
       <Router>
         <Navbar />
+        
         <Routes>
           <Route path="/V2/auth/sign_up" element={<Signup />} />
           <Route path="/V2/auth/sign_in" element={<Signin />} />

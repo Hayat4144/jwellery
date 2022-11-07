5dd040602b53bd43a75dd7e708bd9e754a6d9fb9
hayat4144 <ihayat855@gmail.com>
2022-11-07 14:15:06 +0530

    passwordchange file's route is added.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Signup from "./Auth/Signup";
 import Signin from "./Auth/Signin";
 import Navbar from "./MainComponent/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Password_change from './UserSettings/Password_change';
 function App() {
   return (
     <div className='app'>
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/V2/auth/sign_up" element={<Signup />} />
           <Route path="/V2/auth/sign_in" element={<Signin />} />
+          <Route path='V2/settings/change/password' element={<Password_change />} />
         </Routes>
       </Router>
 

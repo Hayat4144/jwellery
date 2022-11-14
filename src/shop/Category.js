b58f405a5002ea4d42a4b9b039d12e54981d6b79
hayat4144 <ihayat855@gmail.com>
2022-11-14 18:17:09 +0530

    create category.

--- src/shop/Category.js
+++ src/shop/Category.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Fetch_Category } from '../Context/action/Category_Action'
 
-export default function Category() {
+export default function Category(props) {
     const [categories, setcategories] = useState([])
+    const dispatch = useDispatch()
     useEffect(() => {
         const getData = async () => {
             await fetch(`http://localhost:8000/product/api/get_all_categories`)
                 .then(async (res) => {
                     const data = await res.json();
+                    const l = dispatch(Fetch_Category(data))
+                    console.log(l)
                     setcategories(data)
                 })
         }
         getData();
     }, [])
     return (
-        <div className='category md:flex space md:space-x-7'>
+        <div className='category flex mx-5 space-x-5 md:space-x-7'>
             {
                 categories.map((category_item) => (
-                    <div className='item' key={category_item.id}>
-                        <li className='category_name my-3 list-none cursor-pointer'
+                    <ul className='item' key={category_item.id}>
+                        <li className='category_name my-3 text-[18px]  hover:text-indigo-900 text-indigo-700 hover:underline list-none cursor-pointer'
                             onClick={() => {
                             }}>{category_item.name}</li>
-                    </div>
+                    </ul>
                 ))
             }
 

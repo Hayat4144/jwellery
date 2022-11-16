4d397faf64af397d392f01d5a5e15ce0859fefdc
hayat4144 <ihayat855@gmail.com>
2022-11-16 11:19:32 +0530

    search route added.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import NotFound from './Component/NotFound';
 import Home from './Component/Home'
 import FetureSkeleton from './shop/FetureSkeleton';
 import ProductPage from './shop/ProductPage';
+import Category_Product from './shop/Category_Product';
+import SearchProduct from './shop/SearchProduct';
 const FeaturesProduct = React.lazy(() => import('./shop/FeaturesProduct'))
 
 function App() {
@@ -27,6 +29,8 @@ function App() {
           <Route path="/V2/auth/sign_in" element={<Signin />} />
           <Route path='V2/settings/change/password' element={<Password_change />} />
           <Route path='V2/Shop/product/:product_id' element={<ProductPage />} />
+          <Route path='V2/Shop/:category_name/products/' element={<Category_Product />} />
+          <Route path='V2/Shop/q/:search' element={<SearchProduct />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Router>

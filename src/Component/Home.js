f79d977fe1962bd8ff56c867e59a9be32a54559a
hayat4144 <ihayat855@gmail.com>
2022-11-19 10:21:06 +0530

    Password changed file deleted.

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,12 +1,24 @@
-import React, { Suspense } from 'react'
+import React, { Fragment, Suspense } from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+// import Password_change from '../settings/Password_change'
+import Cart from '../shop/Cart'
 import FeaturesProduct from '../shop/FeaturesProduct'
+import ChangeEmail from '../settings/User/ChangeEmail'
+import ChangePassword from '../settings/User/ChangePassword'
+import ShippingAddress from '../shop/ShippingAddress'
 
 function Home() {
   return (
-    <div>
-      <FeaturesProduct />
-      
-    </div>
+    <Fragment>
+      <header className='headers'>
+        <Navbar />
+      </header>
+      <main className='h-full w-full px-5'>
+        <FeaturesProduct />
+      </main>
+      {/* <Footer /> */}
+    </Fragment>
   )
 }
 

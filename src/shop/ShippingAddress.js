5fb72d1c76da5a9840cf9d7a46888bf66b13e596
hayat4144 <ihayat855@gmail.com>
2022-11-24 21:29:20 +0530

    shipping address is added in settings.

--- src/shop/ShippingAddress.js
+++ src/shop/ShippingAddress.js
@@ -89,59 +89,59 @@ export default function ShippingAddress(props) {
                     IsValidMobile_No();
                     IsValidPostalCode();
                 }}>
-                    <div className='rounded-md bg-white'>
+                    <div className='rounded-md bg-white py-2'>
                         <div id="message" className={`${CloseMessagebox ? 'hidden' : 'visible'} messages flex bg-green-200 items-center py-2 px-2 mx-4  text-green-700 text-[13px] mt-3 rounded-lg`}>
                             <span className='message '>Hello This is Hayat ilyas from iqra public school.</span>
                             <IoIosCloseCircleOutline fontSize={'25px'} className='cursor-pointer' onClick={CloseMessageboxFunc} />
                         </div>
                         <div className='coloumn-1 md:flex md:items-center'>
                             <div className='mx-4 my-2 Street-line1-field lg:w-[50%]'>
-                                <label htmlFor='Street-line1-input' className='text-sm font-medium text-gray-700'>Street line</label>
-                                <div className='Streetline1-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} placeholder="Enter you Street line" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='Street-line1-input' className='text-sm font-boldtext-gray-700'>Street line</label>
+                                <div className='Streetline1-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} placeholder="Enter you Street line" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                             <div className='mx-4 Street-line2-field lg:w-[50%] '>
-                                <label htmlFor='Street-line2-input' className='text-sm font-medium text-gray-700'>Area or Colony</label>
-                                <div className='Streetline2-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={Area} onChange={AreaChange} placeholder="Enter Area or Colony" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='Street-line2-input' className='text-sm'>Area or Colony</label>
+                                <div className='Streetline2-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={Area} onChange={AreaChange} placeholder="Enter Area or Colony" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                         </div>
 
                         <div className='coloumn-2 md:flex md:items-center'>
                             <div className='mx-4 mobile-field lg:w-[50%] '>
-                                <label htmlFor='mobile-input' className='text-sm font-medium text-gray-700'>Mobile No.</label>
-                                <div className='mobile-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={Mobile_No} onChange={MobileNoChange} placeholder="Enter you Mobile no." required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='mobile-input' className='text-sm '>Mobile No.</label>
+                                <div className='mobile-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={Mobile_No} onChange={MobileNoChange} placeholder="Enter you Mobile no." required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                             <div className='mx-4 Postal-field lg:w-[50%]'>
-                                <label htmlFor='Postal-input' className='text-sm font-medium text-gray-700'>Postal code</label>
-                                <div className='Postal-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={postalcode} onChange={PostalCodeChange} placeholder="Enter your Postal code" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='Postal-input' className='text-sm '>Postal code</label>
+                                <div className='Postal-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={postalcode} onChange={PostalCodeChange} placeholder="Enter your Postal code" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                         </div>
                         <div className='coloumn-3 md:flex md:items-center'>
                             <div className='mx-4 City-field lg:w-[50%]'>
-                                <label htmlFor='City-input' className='text-sm font-medium text-gray-700'>City</label>
-                                <div className='City-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={City} onChange={CityChange} placeholder="Enter you City" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='City-input' className='text-sm'>City</label>
+                                <div className='City-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={City} onChange={CityChange} placeholder="Enter you City" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                             <div className='mx-4 State-field lg:w-[50%] '>
-                                <label htmlFor='State-input' className='text-sm font-medium text-gray-700'>State</label>
-                                <div className='State-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={State} onChange={StateChange} placeholder="Enter your State" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='State-input' className='text-sm '>State</label>
+                                <div className='State-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={State} onChange={StateChange} placeholder="Enter your State" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                         </div>
                         <div className='md:flex md:items-center'>
                             <div className='mx-4 Country-field lg:w-[46%]'>
-                                <label htmlFor='Country-input' className='text-sm font-medium text-gray-700'>Country</label>
-                                <div className='Country-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                                    <input type={'text'} value={Country} onChange={CountryChange} placeholder="Enter your Country" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+                                <label htmlFor='Country-input' className='text-sm'>Country</label>
+                                <div className='Country-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                                    <input type={'text'} value={Country} onChange={CountryChange} placeholder="Enter your Country" required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm placeholder-gray-700 border-none' />
                                 </div>
                             </div>
                         </div>

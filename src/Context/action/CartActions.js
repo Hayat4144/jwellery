4ec56a88b8e98525610cec27094528513d9a6f11
hayat4144 <ihayat855@gmail.com>
2022-11-24 09:55:50 +0530

    Cart reducer functionality completed.

--- src/Context/action/CartActions.js
+++ src/Context/action/CartActions.js
@@ -1,13 +1,28 @@
-export const ADD_TO_CART = (cart_data)=>{
+export const ADD_TO_CART = (cart_data) => {
     return {
-        type:'ADD_TO_CART',
-        payload:cart_data
+        type: 'ADD_TO_CART',
+        payload: cart_data
     }
 }
 
-export const REMOVE_CART = (product_id)=>{
+export const REMOVE_ITEM_FROM_CART = (product_id) => {
     return {
-        type:'REMOVE_CART',
-        payload:product_id
+        type: 'REMOVE_ITEM_FROM_CART',
+        payload: product_id
+    }
+}
+
+export const INCREASE_QUANTITY = (quanity)=>{
+    return {
+        type:'INCREASE_QUANTITY',
+        payload:quanity
+    }
+}
+
+
+export const DECREASE_QUANTITY= (quanity)=>{
+    return {
+        type:'DECREASE_QUANTITY',
+        payload :quanity
     }
 }
--- src/Context/reducer/Cart_reducer.js
+++ src/Context/reducer/Cart_reducer.js
@@ -1,16 +1,49 @@
+import { useSelector } from "react-redux"
+
 const initialstate = {
-    product_item_id :'',
-    product_image:"",
-    size: "",
-    colour:"",
-    quantity: Number(),
+    // product_item_id :'',
+    // product_image:"",
+    // size: "",
+    // colour:"",
+    // quantity: Number(),
+    productItems: []
 }
 
-export const Cart_reducer = (state=initialstate,action)=>{
+export const Cart_reducer = (state = initialstate, action) => {
+
     switch (action.type) {
         case "ADD_TO_CART":
-            return {...state,product_image:action.payload.product_image,product_item_id:action.payload.product_id,size:action.payload.size_value,colour:action.payload.colour_value,quantity:action.payload.quantity}
+            const item = action.payload;
+            const IsItemExist = state.productItems.find((i) => i.product_item_id === item.product_item_id ? true : false)
+            if (IsItemExist) {
+                return {
+                    ...state,
+                    productItems: state.productItems.map((i) =>
+                        i.product_item_id === IsItemExist.product_item_id ? item : i
+                    ),
+                };
+            }
+            else {
+                return {
+                    ...state,
+                    productItems: [...state.productItems, item]
+                }
+            }
+
+        case "REMOVE_ITEM_FROM_CART":
+            return {
+                ...state, productItems: state.productItems.filter((i) => i.product_item_id !== action.payload.product_item_id)
+            }
+        case 'INCREASE_QUANTITY':
+            return {
+                ...state, productItems: state.productItems.map((i) => i.product_item_id === action.payload.product_item_id ? { ...i, quantity: action.payload.quantity } : i)
+            }
+
+        case 'DECREASE_QUANTITY':
+            return {
+                ...state, productItems: state.productItems.map((i) => i.product_item_id === action.payload.product_item_id ? { ...i, quantity: action.payload.quantity } : i)
+            }
         default:
-            return state
+            return state;
     }
 }

2f3c65874ddee19437cb3caaba17d23aa2d2f89d
hayat4144 <ihayat855@gmail.com>
2022-11-24 21:30:37 +0530

    settings routes is added.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Suspense } from 'react';
 import './index.css'
 import Signup from "./Auth/Signup";
 import Signin from "./Auth/Signin";
-import Navbar from "./Component/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from './Component/NotFound';
 import Home from './Component/Home'
 import ProductPage from './shop/ProductPage';
 import SearchProduct from './shop/SearchProduct';
 import Cart from './shop/Cart';
+import UserSettings from './settings/MainSettings/UserSettings.';
+
 const FeaturesProduct = React.lazy(() => import('./shop/FeaturesProduct'))
 
 function App() {
   return (
     <div className='app'>
       <Router>
-        {/* <Navbar /> */}
         <Routes>
           <Route path="/V2/auth/sign_up" element={<Signup />} />
           <Route path="/" element={<Home />} />
@@ -24,7 +24,9 @@ function App() {
           <Route path="/V2/Shop/Cart/" element={<Cart />} />
           {/* <Route path='V2/Shop/:category_name/products/' element={<Category_Product />} /> */}
           <Route path='V2/Shop/q/:search' element={<SearchProduct />} />
+          <Route path='/V2/settings' element={<UserSettings />} />
           <Route path='*' element={<NotFound />} />
+          
         </Routes>
       </Router>
 

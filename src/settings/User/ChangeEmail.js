f7fef055a55ff8dc93cc7138f44732ce159ccbcd
hayat4144 <ihayat855@gmail.com>
2022-11-24 21:29:37 +0530

    change email is added in settings.

--- src/settings/User/ChangeEmail.js
+++ src/settings/User/ChangeEmail.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react'
-import {IoIosCloseCircleOutline} from 'react-icons/io'
+import { IoIosCloseCircleOutline } from 'react-icons/io'
 
-export default function ChangeEmail() {
+export default function ChangeEmail(props) {
   const [newemail, setnewemail] = useState('')
   const [message, setmessage] = useState('')
   const [CloseMessagebox, setCloseMessagebox] = useState(true)
@@ -11,7 +11,7 @@ export default function ChangeEmail() {
   }
 
   // close message box
-  const CloseMessageboxFunc = ()=>{
+  const CloseMessageboxFunc = () => {
     setCloseMessagebox(!CloseMessagebox)
   }
 
@@ -32,33 +32,33 @@ export default function ChangeEmail() {
       })
   }
 
-  
+
 
   return (
     <Fragment>
-      <div className='container mx-auto mb-10'>
+      <div className='mb-10'>
         <div className='mt-5 text-center header-text'>
 
-          <h3 className='px-5 my-4 text-2xl font-semibold'>Change your Email</h3>
+          {/* <h3 className='px-5 my-4 text-2xl font-semibold'>Change your Email</h3> */}
         </div>
         <form onSubmit={(e) => {
           e.preventDefault();
           // SigninFunc()
         }}>
-          <div className='sm:mx-auto sm:w-[50%]  xl:mx-auto xl:w-[25%]  lg:mx-auto lg:w-[30%] signin-form border md:w-[40%] md:m-auto border-light-white rounded-md bg-white mx-8 mb-6'>
-            <div id="message" className={`${CloseMessagebox ?'hidden' :'visible'} messages flex  bg-green-200 items-center py-2 px-2 mx-4  text-green-700 text-[13px] mt-3 rounded-lg`}>
+          <div className={`sm:mx-auto sm:w-[50%]  signin-form border md:${props.md_width}  border-light-white rounded-md bg-white md:${props.marginx} mb-6`}>
+            <div id="message" className={`${CloseMessagebox ? 'hidden' : 'visible'} messages flex  bg-green-200 items-center py-2 px-2 mx-4  text-green-700 text-[13px] mt-3 rounded-lg`}>
               <span className='message '>Hello This is Hayat ilyas from iqra public school.</span>
               <IoIosCloseCircleOutline fontSize={'25px'} className='cursor-pointer' onClick={CloseMessageboxFunc} />
             </div>
             <div className='py-4 mx-4 email-field '>
-              <label htmlFor='email-input' className='text-sm font-medium text-gray-700'>Email</label>
-              <div className='email-input border border-gray-200 rounded-md my-2 py-[2px]'>
-                <input type={'email'} placeholder="Enter you email" value={newemail} onChange={newemailChange} required className='bg-inherit w-[18.8em] px-2 py-[4px] outline-none text-sm text-gray-700 border-none' />
+              <label htmlFor='email-input' className='text-sm'>Email</label>
+              <div className='email-input border border-gray-300 rounded-md my-2 py-[2px]'>
+                <input type={'email'} placeholder="Enter you email" value={newemail} onChange={newemailChange} required className='bg-inherit w-[18.8em] px-2 py-[4px] placeholder-gray-700 outline-none text-sm border-none' />
               </div>
             </div>
 
-            <div className='sumbit-btn mx-4 w-[20em]mb-5'>
-              <button type='submit' className='w-full h-8 px-6 mb-8 text-center text-white outline-none text-bold bg-slate-800 rounded-md hover:bg-slate-900'>Submit</button>
+            <div className='sumbit-btn mx-4 mb-5'>
+              <button type='submit' className={`md:w-24 ${props.button_width} h-8 px-6 mb-8 text-center text-white outline-none text-bold ${props.button_background} rounded-md hover:${props.button_hover}`}>Submit</button>
             </div>
           </div>
         </form>

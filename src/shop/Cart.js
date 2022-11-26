59be3d0f36e775b891b3e14769388e4297ebd85d
hayat4144 <ihayat855@gmail.com>
2022-11-26 22:07:04 +0530

    shoping_cart total price count including other charges.

--- src/shop/Cart.js
+++ src/shop/Cart.js
@@ -5,16 +5,27 @@ import { AiOutlineMinus } from 'react-icons/ai'
 import { useDispatch, useSelector } from 'react-redux'
 import Navbar from '../Component/Navbar'
 import { Helmet } from 'react-helmet'
+import { BiShoppingBag } from 'react-icons/bi'
 
 export default function Cart() {
-
     const Cartdata = useSelector(state => state.Cart_reducer.productItems)
     const [minimumquantity, setminimumquantity] = useState(10)
     const [quantityalert, setQuantityalert] = useState(false)
+    const [shippingestimate, setShippingestimate] = useState(200)
+    const [tax_percentage, settax_percentage] = useState(0.5)
 
     const dispatch = useDispatch()
-    console.log(Cartdata)
 
+    // getting total price of product in cart
+    const subtotal = Cartdata.reduce((a, b) => {
+        return a.price * a.quantity + b.price * b.quantity;
+
+    })
+
+    // tax applied according to the total price of shopping
+    const tax_amount = Math.trunc(tax_percentage / 100 * subtotal);
+
+    const total_amount = tax_amount + subtotal + shippingestimate;
 
     const Product_image = [
         {
@@ -41,8 +52,8 @@ export default function Cart() {
             setQuantityalert(true)
         }
         else {
-            const l = dispatch({ type: 'INCREASE_QUANTITY', payload: { 'product_item_id': id, 'quantity': newquantity } })
-            console.log(l)
+            dispatch({ type: 'INCREASE_QUANTITY', payload: { 'product_item_id': id, 'quantity': newquantity } })
+
         }
     }
 
@@ -52,18 +63,18 @@ export default function Cart() {
             setQuantityalert(true)
         }
         else {
-            const l = dispatch({ type: 'DECREASE_QUANTITY', payload: { 'product_item_id': id, 'quantity': newquantity } })
-            console.log(l)
+            dispatch({ type: 'DECREASE_QUANTITY', payload: { 'product_item_id': id, 'quantity': newquantity } })
+
         }
     }
     const RemoveCartItem = (id) => {
-
         dispatch({
             type: "REMOVE_ITEM_FROM_CART", payload: {
                 "product_item_id": id
             }
         })
     }
+
     return (
         <Fragment>
             <Helmet>
@@ -88,70 +99,80 @@ export default function Cart() {
 
                     </div>
                 </Fragment> : <Fragment>
-                    <main className='cart-page-container grid grid-cols-1 md:grid-cols-2 max-w-7xl gap-5'>
-                        <section className='product-list-cart mx-3 my-3 rounded-lg bg-white px-3 lg:ml-24 lg:w-full'>
-                            <h3 className='page-title text-xl font-[500] my-2 cursor-pointer'> Your Cart</h3>
+                    <h1
+                        className='page-indicator flex 
+                        justify-center my-5 space-x-5 cursor-pointer tracking-tight'>
+                        <BiShoppingBag fontSize={'30px'} className='hover:text-indigo-700' />
+                        <span className='Page-title font-semibold
+                        text-2xl hover:text-indigo-800'>My Cart</span>
+                    </h1>
+                    <main className='grid grid-cols-1 md:grid-cols-3 mx-2 gap-10 md:gap-5'>
+                        <section className='product-items-details md:col-span-2 rounded-lg'>
+                            <div className='product-top-banner flex text-white bg-indigo-700 h-10 items-center px-2 justify-between'>
+                                <h1 className='product-item cursor-pointer'>Product</h1>
+                                <h1 className='proudct-banner-quantity hidden md:block cursor-pointer ml-24'>Quantity</h1>
+                                <h1 className='proudct-banner-price cursor-pointer'>Each Price</h1>
+
+                            </div>
                             {
-                                Cartdata.map((item, index) => (
-                                    <article className='product&details py-2 flex space-x-2' key={item.product_item_id}>
-
-                                        <figure className={`image`} key={item.product_item_id} >
-                                            <img alt='product-img' src={item.product_image} className='max-w-[100%] rounded-xl h-20 w-32  ' />
-                                        </figure>
-                                        <div className='detailas w-full pl-3'>
-                                            <h2 className='product-name flex items-center'>
-                                                <p className='remove-product-icon w-full font-medium pr-2'>{item.product_name}</p>
-                                                <IoIosCloseCircleOutline className='cursor-pointer' fontSize={'22px'} onClick={() => RemoveCartItem(item.product_item_id)} />
-                                            </h2>
-                                            <div className='size&colour flex space-x-10 items-center'>
-                                                <h3 className='size flex space-x-3 items-center  '>
-                                                    <small className='value text-sm text-gray-600'>{item.size}</small>
-                                                </h3>
-                                                <h3 className='colour flex space-x-3 items-center'>
-                                                    <small className='value text-sm text-gray-600'>{item.colour}</small>
-                                                </h3>
-                                            </div>
-                                            <h3 className='price text-[15px] flex space-x-10  items-center my-2'>
-                                                <span className='font-bold'>{item.price}</span>
-                                                <div className='quanity-value flex lg:mx-0  w-full  items-center space-x-2'>
-                                                    <button className='decrease-btn' disabled={item.quanity == 10 || item.quanity < 10 ? true : false} onClick={() => { DescreaseQuantity(item.product_item_id, item.quantity) }} ><AiOutlineMinus fontSize={'22px'} color={'black'} onClick={() => { }} /></button>
-                                                    <input type={'text'} className='w-10 border border-gray-300 outline-none bg-transparent text-black px-1 text-center' value={item.quantity} onChange={(e) => {
-
-                                                    }} />
-                                                    <BiPlus fontSize={'22px'} color={'black'} onClick={() => { IncreaseQuantity(item.product_item_id, item.quantity) }} />
+                                Cartdata.map((item) => (
+                                    <div className='product-image&detailst px-2 my-5' key={item.product_item_id}>
+                                        <div className='product-details flex space-x-2'>
+                                            <figure className='product-imaga-contianer overflow-hidden w-[20%] rounded-md'>
+                                                <img src="/images/thumbnail-1.jpg" className='h-28 object-center object-cover w-full hover:scale-125 rounded-md ' />
+                                            </figure>
+                                            <div className='product-name&quantity px-2 w-[60%] grid grid-cols-1 md:grid-cols-2 '>
+                                                <div className='product-info'>
+                                                    <div className='name-info-container lg:px-5'>
+                                                        <h2 className='product-name '>{item.product_name}</h2>
+                                                        <h2 className='Colour'> Colour<span className='mx-2 text-sm text-gray-700'>{item.colour}</span></h2>
+                                                        <h2 className='size'> Size<span className='mx-2 text-sm text-gray-700'>{item.size}</span></h2>
+                                                    </div>
                                                 </div>
-                                                <span className='text-xl'>{item.price * item.quantity}</span>
-                                            </h3>
-                                            <span id='quantity-limit' className={`text-sm text-red-700 ${item.quanity < 10 ? 'block' : 'hidden'}`}>You should have to buy at least 10 pcs.</span>
-
-
+                                                <div className='product-quantity-show my-2 bg-gray-100 flex items-center justify-between px-2 h-8 rounded-md'>
+                                                    <button className='decrease-btn bg-indigo-800 rounded-full text-white' disabled={item.quanity == 10 || item.quanity < 10 ? true : false} onClick={() => { DescreaseQuantity(item.product_item_id, item.quantity) }} ><AiOutlineMinus fontSize={'22px'} onClick={() => { }} /></button>
+                                                    <input type={'text'} className='w-10 border border-indigo-800 outline-none bg-transparent text-black px-1 text-center' value={item.quantity} onChange={(e) => {
+                                                    }} />
+                                                    <button className='increase-button text-white bg-indigo-800 rounded-full'>
+                                                        <BiPlus fontSize={'22px'} onClick={() => { IncreaseQuantity(item.product_item_id, item.quantity) }} />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <div className='each-price text-end ml-32 sm:w-[23%]'>
+                                                <h2 className='price'>Rs <span className='price-vlaue'>{item.price}</span></h2>
+                                            </div>
                                         </div>
-                                    </article>
-
-
+                                        <div className='total-price-of-individual-product flex items-center my-2 border-b borde-gray-300 pb-5 justify-between'>
+                                            <h2 className='product-subtotal-text text-xl'>Total</h2>
+                                            <h2 className='product-total-price'> Rs <span className='total-price-value'>{item.price * item.quantity}</span></h2>
+                                        </div>
+                                    </div>
                                 ))
                             }
+
                         </section>
-                        <section className='subtotal md:h-[60%] lg:w-[70%]  rounded-lg shadow-lg lg:ml-36 bg-white my-2  px-3 mx-5'>
-                            <h3 className='order-summary-text text-xl my-4'>Order Summary</h3>
+                        <section className='subtotal mx-4 shadow-lg md:h-[60%] px-2 bg-gray-100 rounded-md my-5 md:my-0 '>
+                            <h3 className='order-summary-text py-2 text-xl text-indigo-700'>Order Summary</h3>
                             <div className='subtotal flex items-center border-b border-slate-300 pb-3 my-2 justify-between'>
-                                <h3 className='subtotaltext text-xl text-gray-600'>Subtotal</h3>
-                                <span className='amount'>Rs 4034</span>
+                                <h3 className='subtotaltext'>Subtotal</h3>
+                                <span className='amount'>Rs {subtotal}</span>
                             </div>
                             <div className='shiping-estimate  my-2 border-b border-slate-300 pb-3 items-center justify-between flex'>
-                                <h3 className='shipingtext text-xl text-gray-600 '>Shipping estimate</h3>
+                                <h3 className='shipingtext'>Shipping estimate</h3>
                                 <span className='amount'>Rs 200</span>
                             </div>
                             <div className='tax-estimate items-center border-b border-slate-300 pb-3 my-2 justify-between flex'>
-                                <h3 className='tax-text text-xl text-gray-600'>Tax estimate</h3>
-                                <span className='amount'>Rs 200</span>
+                                <h3 className='tax-text '>Tax estimate</h3>
+                                <span className='amount'>Rs {tax_amount}</span>
                             </div>
-                            <div className='total flex items-center pb-2 justify-between'>
-                                <h3 className='order-total text-xl text-gray-600 '>Total</h3>
-                                <span className='amount'>Rs 5600</span>
+                            <div className='total flex items-center pb-2 justify-between mb-5'>
+                                <h3 className='order-total'>Total</h3>
+                                <span className='amount'>Rs {total_amount}</span>
                             </div>
                         </section>
                     </main>
+
+
                 </Fragment>
             }
 
